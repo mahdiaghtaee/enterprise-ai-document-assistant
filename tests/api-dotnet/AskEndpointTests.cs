e48515aa2f4123cbece7c2eaa7338fb003046d48
Mahdi Aghtaee <44207275+mahdiaghtaee@users.noreply.github.com>
2026-08-03 11:46:35 +0330

    Fix Ask provider failure test import

--- tests/api-dotnet/AskEndpointTests.cs
+++ tests/api-dotnet/AskEndpointTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Json;
 using EnterpriseDocumentAssistant.Api.Documents;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
